98272e996868570bc9372cfc3d058849b392b615
github1698 <titusyemisi@yahoo.com>
2024-08-07 18:10:06 +0100

    done implementing login

---
+++ member/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    
+    #path('',views.register_user,name='register_user'),    
+    #path('login',views.login_user,name='signin'),
+    path('',views.signup,name='register_user'),
+    path('logout_user',views.logout_user,name='logout'),
+    path('user_login',views.user_login,name='signin'),
+    
+    #path('sign_up/',views.Logout,name='logout'),
+    #path('sign_up/',views.Login,name='login'),
+
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "authentical/reset_password.html"), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "authentical/password_reset_sent.html"), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "authentical/password_reset_form.html"), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "authentical/passsword_reset_done.html"), name ='password_reset_complete')
+    
+ 
+]
+
+
